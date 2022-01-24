55c7679192e9dfe3200ef763a19abd91e3219735
radek <radek109@o2.pl>
2022-01-24 20:02:32 +0100

    Dodaj pliki projektów.

---
+++ ProgramInwentaryzacyjny/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows;
+
+namespace ProgramInwentaryzacyjny
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Magazyn_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new StorageMainPage();
+        }
+        private void Wyjście_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
